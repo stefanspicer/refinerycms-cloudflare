809f9c24dd8b9be41e6dc0b905f5c9fd4d9c9872
Stefan <stefan@glasscanvas.io>
2016-11-04 12:46:13 -0700

    add cloudflare_domain setting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Refinery::Setting.get_or_set(:cloudflare_domain,       "", {form_value_type: 'string'})
 Refinery::Setting.get_or_set(:cloudflare_auth_email,   "", {form_value_type: 'string'})
 Refinery::Setting.get_or_set(:cloudflare_auth_api_key, "", {form_value_type: 'string'})
 Refinery::Setting.get_or_set(:cloudflare_zone,         "", {form_value_type: 'string'})
